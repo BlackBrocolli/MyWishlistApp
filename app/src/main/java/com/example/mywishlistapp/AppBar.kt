58d0427e86d117df64bffa172ee39171aef5ff45
BlackBrocolli <201111011@mhs.stiki.ac.id>
2024-03-20 14:19:51 +0700

    Add ArrowBack Icon to TopAppBar

--- app/src/main/java/com/example/mywishlistapp/AppBar.kt
+++ app/src/main/java/com/example/mywishlistapp/AppBar.kt
@@ -2,10 +2,15 @@ package com.example.mywishlistapp
 
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -14,6 +19,23 @@ fun AppBarView(
     title: String,
     onBackNavClicked: () -> Unit = {}
 ) {
+    val navigationIcon: (@Composable () -> Unit)? =
+        if (!title.contains("Wishlist")) {
+            {
+                IconButton(onClick = { onBackNavClicked() }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Arrow Back Icon",
+                        tint = Color.White
+                    )
+                }
+            }
+
+        } else {
+            null
+        }
+
+
     TopAppBar(
         title = {
             Text(
@@ -26,6 +48,6 @@ fun AppBarView(
         },
         elevation = 3.dp,
         backgroundColor = colorResource(id = R.color.app_bar_color),
-//        navigationIcon =
+        navigationIcon = navigationIcon
     )
 }
