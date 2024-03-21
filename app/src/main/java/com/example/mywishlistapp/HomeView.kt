18ea91655a315bf6c5be1fe5b25edb63086feb42
BlackBrocolli <201111011@mhs.stiki.ac.id>
2024-03-21 11:36:45 +0700

    Add Navigation composable which contain NavHost

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -1,17 +1,28 @@
 package com.example.mywishlistapp
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.mywishlistapp.data.DummyWish
+import com.example.mywishlistapp.data.Wish
 
 @Composable
 fun HomeView() {
@@ -39,7 +50,32 @@ fun HomeView() {
                 .fillMaxSize()
                 .padding(it)
         ) {
+            items(DummyWish.wishlist) { wish ->
+                WishItem(wish = wish) {
 
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WishItem(wish: Wish, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+            .clickable { onClick() },
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = wish.title, fontWeight = FontWeight.ExtraBold)
+            Text(text = wish.description)
         }
     }
 }
